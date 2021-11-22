bb57c350965b2cf763863cf8136afe2801a33fed
ITITIU20347 <ITITIU20347@student.hcmiu.edu.vn>
2021-11-22 16:07:51 +0700

    uio

--- src/Exercise4.c
+++ src/Exercise4.c
@@ -8,31 +8,38 @@ Ex:
 |________________________________________________|
 */
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
 void Ex4(char *str){
-	int i=0,j;
+	//Your codes here
+	int i;
     int n = strlen(str);
-    for(i=0;i<n;i++)
-     if(str[i]==' ')
-      {
-        j=i;
-           while(j<n-1)
-           {
-            str[j] = str[j+1];
-            j++;
-           }
-        n--;
-        i--;
-      }
-      str[n]='\0';
-    printf(" %s ",str);
-	
+    for(i = 0; i < n; i++){ 
+        if(str[i] == ' '){
+            int nBegin = i;
+ 
+            if(str[i + 1] == ' '){
+                nBegin = i + 1;
+            }
+ 
+            if(str[i + 1] == ' ' || i == 0 || i == n - 2){
+                for(int j = nBegin; j < n - 1; j++){
+                    char cTemp = str[j];
+                    str[j] = str[j + 1];
+                    str[j + 1] = cTemp;
+                }
+                i--;
+                n--;
+            }
+        }
+    }
+    str[n] = '\0';
+    printf("%s ",str);
 }
-
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	char *testcase = argv[1];
